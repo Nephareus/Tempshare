1c37e0337f982a300a75eaeecdfd1c54fc422974
Nephareus <70549122+Nephareus@users.noreply.github.com>
2020-11-25 20:26:21 -0500

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float Speed = 10f;
+
+    public Rigidbody2D rb;
+
+    // bool for changing to animation for pushing items
+    public bool isPushing = false;
+
+    public Animator anim;
+    private Vector2 vel;
+    private Vector2 direction;
+
+    private void Update()
+    {
+        anim.SetFloat("Horizontal", vel.x);
+        anim.SetFloat("Vertical", vel.y);
+        anim.SetFloat("Speed", vel.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        {
+            rb.MovePosition(rb.position + vel * Speed * Time.fixedDeltaTime);
+        }
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+
+        vel = context.ReadValue<Vector2>();
+    }
+}
